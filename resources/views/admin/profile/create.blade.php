30837306b55a7289c3179f748466296a375bbb33
Takada <tomoco.102392@gmail.com>
2020-08-23 11:36:37 +0000

    PHP10Kadai(create)

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta carset= "utf-8">
+        <meta http-equiv= "X-UA-Compatible" content= "IE-edge">
+        <meta name= "viewport" content= "width=device-width, initial-scale=1">
+        
+        <title>Profile</title>
+        
+    </head>
+    <body>
+        <h1>プロフィール作成</h1>
+        
+    </body>
+    
+    
+</html>
