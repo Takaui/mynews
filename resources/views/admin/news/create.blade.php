26ed7078dd269903cc16ee72fc0cbffc6eee0ccd
Takada <tomoco.102392@gmail.com>
2020-08-30 09:35:23 +0000

    PHP11Kadai

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta carset= "utf-8">
-        <meta http-equiv= "X-UA-Compatible" content= "IE-edge">
-        <meta name= "viewport" content= "width=device-width, initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-        
-        //記述箇所不明（PHP11文中）
-        {{-- layouts/admin.blade.phpを読み込む --}}
-        @extends('layots.admin')
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
         
         {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
         @section('title','ニュースの作成')
@@ -22,10 +9,8 @@
             <div class= "container">
                 <div class= "row">
                     <div class="col-md-8 mx-auto">
-                        <h2>ニュース作成</h2>
+                        <h2>ニュース新規作成</h2>
                     </div>
                 </div>
             </div>
-        @endsection
-    </body>
-</html>
+ @endsection
