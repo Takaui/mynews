42bc9db84836a5295da20e5d71554665f43cff9f
Takada <tomoco.102392@gmail.com>
2020-09-15 11:33:34 +0000

    Kadaiデプロイ

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="container">
+        <p>変更</p>
         <hr color="#c0c0c0">
         @if (!is_null($headline))
             <div class="row">
@@ -12,7 +13,7 @@
                                 <div class="image">
                                     @if ($headline->image_path)
                                         <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif    
+                                    @endif
                                 </div>
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
@@ -20,7 +21,7 @@
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body,650) }}</p>
+                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
                         </div>
                     </div>
                 </div>
@@ -31,16 +32,16 @@
             <div class="posts col-md-8 mx-auto mt-3">
                 @foreach($posts as $post)
                     <div class="post">
-                        <div class-"row">
+                        <div class="row">
                             <div class="text col-md-6">
                                 <div class="date">
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
                                 <div class="title">
-                                    {{ str_limit($post->title,150) }}
+                                    {{ str_limit($post->title, 150) }}
                                 </div>
                                 <div class="body mt-3">
-                                    {{ str_limit($post->body,1500) }}
+                                    {{ str_limit($post->body, 1500) }}
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
